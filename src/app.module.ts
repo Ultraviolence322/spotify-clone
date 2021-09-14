d1ee2adbdbd023c319fddceaf1c90efa542a9a04
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-14 16:48:32 +0300

    added mongoose to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [TrackModule]
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.4y5pr.mongodb.net/spotify-clone?retryWrites=true&w=majority'),
+    TrackModule,
+  ]
 })
 
 export class AppModule {}
