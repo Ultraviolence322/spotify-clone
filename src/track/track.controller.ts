9e2078d9472ee9a2323c9c480303b7267c8b1447
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-15 08:47:58 +0300

    added picture and audio params to create track method in track.controller

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, UploadedFiles, UseInterceptors } from "@nestjs/common";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
 import { ObjectId } from "mongoose";
 import { CreateCommentDto } from "./dto/create-comment.dto";
 import { CreateTrackDto } from "./dto/create-track.dto";
@@ -12,8 +13,14 @@ export class TrackController {
   }
 
   @Post()
-  create(@Body() dto: CreateTrackDto) {
-    return this.trackService.create(dto)
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'picture', maxCount: 1 },
+    { name: 'audio', maxCount: 1 },
+  ]))
+  create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+    const {picture, audio} = files
+    
+    return this.trackService.create(dto, picture[0], audio[0])
   }
 
   @Get()
