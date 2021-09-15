71cb800c815c51a6ec15234a1a6acba428546061
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-15 08:43:19 +0300

    import ServeStaticModule and FileModule in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
+import { FileModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, 'static'),
+    }),
     MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.4y5pr.mongodb.net/spotify-clone?retryWrites=true&w=majority'),
     TrackModule,
+    FileModule
   ]
 })
 
