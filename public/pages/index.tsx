3bab76ddb88728a74c02c5579b9c708d8ef35bbe
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-20 09:39:19 +0300

    added HomePage component

---
+++ public/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MainLayout from "../components/MainLayout";
+
+export default function HomePage() {
+  return (
+    <MainLayout>
+      <h1>Welcome to Spotify clone!</h1>
+    </MainLayout>
+  )
+}
