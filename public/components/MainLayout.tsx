316c069d8ff7839450322c50b3476e29df498138
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-20 15:36:55 +0300

    added TrackLine in MainLayout

--- public/components/MainLayout.tsx
+++ public/components/MainLayout.tsx
@@ -3,18 +3,37 @@ import React, { ReactElement, ReactNode } from 'react'
 import { Container } from '@mui/material'
 
 import Navbar from './Navbar'
+import TrackLine from './Player'
 
 interface Props {
   children: ReactNode
 }
 
 export default function MainLayout({children}: Props): ReactElement {
+  const track = {
+    _id: '1',
+    artist: 'Artist 1',
+    audio: '',
+    comments: [
+      {
+        _id: '1',
+        username: 'Vlad',
+        text: 'nice song'
+      },
+    ],
+    listens: 0,
+    name: 'Track 1',
+    picture: 'http://localhost:5000/image/1.jpg',
+    text: 'Desc'
+  }
+
   return (
     <div>
       <Navbar />
       <Container>
         {children}
       </Container>
+      <TrackLine isActive={false} track={track} />
     </div>
   )
 }
