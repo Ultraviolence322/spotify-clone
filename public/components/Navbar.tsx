fe148b08547f51cb199ceb42638475ba67c6ca89
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-20 09:38:48 +0300

    added links to Navbar component

--- public/components/Navbar.tsx
+++ public/components/Navbar.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
@@ -15,10 +12,25 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import { useRouter } from 'next/dist/client/router';
 
 const drawerWidth = 240;
+
+const links = [
+  {
+    name: 'Home',
+    route: '/'
+  },
+  {
+    name: 'Tracks',
+    route: '/tracks'
+  },
+  {
+    name: 'Albums',
+    route: '/albums'
+  }
+]
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -31,6 +43,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 
 export default function PersistentDrawerLeft() {
   const theme = useTheme();
+  const router = useRouter();
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -54,7 +67,7 @@ export default function PersistentDrawerLeft() {
           <MenuIcon />
         </IconButton>
         <Typography variant="h6" noWrap component="div">
-          Persistent drawer
+          Spotify Clone
         </Typography>
       </Toolbar>
       <Drawer
@@ -77,12 +90,14 @@ export default function PersistentDrawerLeft() {
         </DrawerHeader>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
+          {links.map((link, index) => (
+            <ListItem button key={index} onClick={() => {
+              router.push(link.route)
+            }}>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <MailIcon />
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={link.name} />
             </ListItem>
           ))}
         </List>
