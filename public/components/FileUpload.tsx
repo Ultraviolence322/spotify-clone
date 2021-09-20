fada40cba7e9ba37ef8a79e55575c8afff453e7d
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-20 15:28:48 +0300

    added FileUpload component

---
+++ public/components/FileUpload.tsx
@@ -0,0 +1,29 @@
+import React, { ChangeEvent, ReactElement, ReactNode, useRef } from 'react'
+
+interface Props {
+  children: ReactNode,
+  accept: string,
+  setFile: Function
+}
+
+export default function FileUpload({children, accept, setFile}: Props): ReactElement {
+  const fileRef = useRef<HTMLInputElement>() 
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log('e', e);
+    setFile(e.target.files[0])
+  }
+
+  return (
+    <div onClick={() => fileRef.current.click()}>
+      <input 
+        type="file" 
+        ref={fileRef}
+        style={{display: 'none'}}
+        onChange={changeHandler}
+        accept={accept}
+      />
+      {children}
+    </div>
+  )
+}
