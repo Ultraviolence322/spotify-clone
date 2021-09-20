f126049a26008dd5fdcadd855e8f332cafe0e80f
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-20 09:37:22 +0300

    added TrackPage component

---
+++ public/pages/tracks/[id].tsx
@@ -0,0 +1,52 @@
+import { TextField } from '@mui/material'
+import React, { ReactElement } from 'react'
+import MainLayout from '../../components/MainLayout'
+
+interface Props {
+  
+}
+
+export default function TrackPage({}: Props): ReactElement {
+  const track = {
+    _id: '1',
+    artist: 'Artist 1',
+    audio: '',
+    comments: [
+      {
+        _id: '1',
+        username: 'Vlad',
+        text: 'nice song'
+      },
+    ],
+    listens: 0,
+    name: 'Track 1',
+    picture: 'http://localhost:5000/image/1.jpg',
+    text: 'Desc'
+  }
+  return (
+    <MainLayout>
+      <img src={track.picture} width="200" height="200" alt="picture" />
+      <h1>{track.name}</h1>
+      <div>listens: {track.listens}</div>
+      <div>desc: {track.text}</div>
+
+      <h2>Comments</h2>
+      <TextField 
+        label="Name"
+      />
+      <TextField 
+        label="Comment"
+        multiline
+      />
+
+      <ul>
+        {track.comments.map(c => {
+          return <li>
+            <h3>{c.username}</h3>
+            <p>{c.text}</p>
+          </li>
+        })}
+      </ul>
+    </MainLayout>
+  )
+}
