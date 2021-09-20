686b49d39e0eb2694f84df3b1314025fb50959aa
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-20 09:40:31 +0300

    added AlbumPage component

---
+++ public/pages/albums/index.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react'
+import MainLayout from '../../components/MainLayout'
+
+interface Props {
+  
+}
+
+export default function AlbumsPage({}: Props): ReactElement {
+  return (
+    <MainLayout>
+      <h1>Albums !</h1>
+    </MainLayout>
+  )
+}
