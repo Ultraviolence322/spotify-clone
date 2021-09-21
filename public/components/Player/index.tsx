c568d354cf54ccb9c3584b7b72a4b8f8c3584c2a
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-21 16:46:39 +0300

    added redux in Player

--- public/components/Player/index.tsx
+++ public/components/Player/index.tsx
@@ -1,44 +1,73 @@
 import { Pause, PlayArrow, VolumeUp } from '@mui/icons-material'
 import { IconButton } from '@mui/material'
-import React, { ReactElement, useState } from 'react'
-import { ITrack } from '../../interfaces'
+import React, { ReactElement, useEffect, useState } from 'react'
+import { useActions } from '../../hooks/useActions'
+import { useTypedSelector } from '../../hooks/useTypeSelector'
 import styles from '../../styles/Player.module.scss'
 import TrackProgress from './TrackProgress'
 
-interface Props {
-  isActive: boolean,
-  track: ITrack
-}
+let audio = null;
 
-export default function TrackLine({isActive, track}: Props): ReactElement {
-  const [volume, setVolume] = useState(50)
-  const [trackTime, setTrackTime] = useState(0)
+export default function TrackLine(): ReactElement {
+  const {active, currentTime, duration, pause, volume } = useTypedSelector(state => state.player)
+  const {pauseTrack, playTrack, setVolumeTrack, setCurrentTimeTrack, setDurationTrack} = useActions()
+
+  const play = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.stopPropagation()
+
+    if (pause) {
+      playTrack()
+      audio.play()
+    } else {
+      pauseTrack()
+      audio.pause()
+    }
+  }
+
+  const changeVolume = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setVolumeTrack(+e.target.value)
+    audio.volume = +e.target.value / 100
+  }
+
+  const changeCurrentTime = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setCurrentTimeTrack(+e.target.value)
+    audio.currentTime = +e.target.value
+  }
+
+  useEffect(() => {
+    audio = new Audio()
+    audio.src = 'http://localhost:5000/audio/cfa05a9b-1bc6-4c9b-8ea9-48d4876c0ae2.mp3'
+    audio.volume = volume / 100
+
+    audio.onloadedmetadata = () => setDurationTrack(audio.duration)
+    audio.ontimeupdate = () => setCurrentTimeTrack(audio.currentTime)
+  }, [])
 
   return (
     <div className={styles.player}>
-      <IconButton>
+      <IconButton onClick={play}>
         {
-          isActive 
-            ? <Pause/>
-            : <PlayArrow/>
+          pause 
+            ? <PlayArrow/>
+            : <Pause/>
         }
       </IconButton>
       <img
-        src={track.picture}
+        src={''}
         width={70}
         alt="track" 
       />
       <div>
-        {track.name}
+        {'vlad\'s song'}
       </div>
       <div>
-        {track.artist}
+        {'vl;af'}
       </div>
-      <TrackProgress left={volume} right={100} onChange={() => ({})} />
+      <TrackProgress left={currentTime} right={duration} onChange={changeCurrentTime} />
       <IconButton>
         <VolumeUp />
       </IconButton>
-      <TrackProgress left={trackTime} right={100} onChange={() => ({})} />
+      <TrackProgress left={volume} right={100} onChange={changeVolume} />
     </div>
   )
 }
