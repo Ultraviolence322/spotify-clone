9f6d7abd26f474416660db3b4d2c82a9127f40e6
Ultraviolence322 <segeichenkov18@gmail.com>
2021-09-21 19:18:19 +0300

    added thunk middleware

--- public/store/index.ts
+++ public/store/index.ts
@@ -1,7 +1,10 @@
-import {createStore, AnyAction, Store} from 'redux';
-import {createWrapper, Context, HYDRATE, MakeStore} from 'next-redux-wrapper';
+import {createStore, Store, applyMiddleware, AnyAction} from 'redux';
+import {createWrapper, Context, MakeStore} from 'next-redux-wrapper';
 import { reducer, RootState } from './reducers';
+import thunk, { ThunkDispatch } from 'redux-thunk';
 
-const makeStore: MakeStore<Store<RootState>> = (context: Context) => createStore(reducer);
+const makeStore: MakeStore<Store<RootState>> = (context: Context) => createStore(reducer, applyMiddleware(thunk));
 
 export const wrapper = createWrapper<Store<RootState>>(makeStore, {debug: true});
+
+export type NextThunkDispatch = ThunkDispatch<RootState, void, AnyAction>
